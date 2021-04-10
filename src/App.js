af7b075313b3b2835ca688a4d8aaba7a869364dd
SuhasM92 <suhasmohan1992@gmail.com>
2021-04-10 18:04:51 -0500

    changed app txt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Bye Bye Suhas!
+           Hi! Welcome to Chili's 
         </p>
         <a
           className="App-link"
